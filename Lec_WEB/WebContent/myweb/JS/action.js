21de47c9acf3312c3567a081d21ef06236647bd9
Jay9011 <whdtjq5904@gmail.com>
2020-05-12 10:30:46 +0900

    200512

---
+++ Lec_WEB/WebContent/myweb/JS/action.js
@@ -0,0 +1,5 @@
+let download_link = 'https://store.steampowered.com/about/?snr=1_4_4__global-header';
+let download = document.getElementById("header_download");
+download.addEventListener("click", function(){
+    location.href = download_link;
+});
